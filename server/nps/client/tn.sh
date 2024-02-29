2f878a9b2a75c3a3cc2efec278248849839311dd
wendy77 <wendyno01@outlook.com>
2024-02-29 04:20:32 +0000

    update server/nps/client/tn.sh.
    Signed-off-by: wendy77 <wendyno01@outlook.com>

--- server/nps/client/tn.sh
+++ server/nps/client/tn.sh
@@ -1,5 +1,4 @@
 pkill npc
-cd /opt/npc
-nohup npc /opt/npc >/dev/null 2>&1 &./npc install 
--server=mianyang.wendy-ip.com:8024 -vkey=wendy123 -type=tcp
-sudo ./npc start
+nohup npc /opt/npc >/dev/null 2>&1 &/opt/npc/npc install -server=
+#mianyang.wendy-ip.com:8024 -vkey=wendy123
+# -type=tcp && sudo /opt/npc/npc start
